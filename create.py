f949d5aa9c54d9f4e8abb406016eb3155cd422dc
kamden-rasmussen <kamdenr@gmail.com>
2023-02-13 13:21:47 -0700

    create looking good

--- create.py
+++ create.py
@@ -4,10 +4,10 @@ def createTables():
     conn = sqlite3.connect('mydb.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS users
-                 (id INTEGER PRIMARY KEY, name TEXT, email TEXT, password TEXT)''')
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)''')
     c.execute('''CREATE TABLE IF NOT EXISTS posts
-                 (id INTEGER PRIMARY KEY, title TEXT, body TEXT, user_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id))''')
-    c.execute('''CREATE TABLE IF NOT EXISTS comments
-                 (id INTEGER PRIMARY KEY, body TEXT, user_id INTEGER, post_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id), FOREIGN KEY(post_id) REFERENCES posts(id))''')
+                 (id INTEGER PRIMARY KEY, body TEXT, user_id INTEGER, created_at DATETIME, FOREIGN KEY(user_id) REFERENCES users(id))''')
+    c.execute('''CREATE TABLE IF NOT EXISTS friends
+                    (user_id INTEGER, friend_id INTEGER, FOREIGN KEY(user_id) REFERENCES users(id), FOREIGN KEY(friend_id) REFERENCES users(id))''')
     conn.commit()
     conn.close()
---
+++ main.py
@@ -0,0 +1,9 @@
+import create
+
+def main():
+    create.createTables()
+    return 0
+
+
+if __name__ == '__main__':
+    main()
