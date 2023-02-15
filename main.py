d4363408ea442c5064b9bb07d70514db50b4b0f0
kamden-rasmussen <kamdenr@gmail.com>
2023-02-15 15:57:42 -0700

    introducing usersService and refactoring creation

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import create
+import sqlite3
+import os
+from users import UsersService
 
 def main():
-    create.main()
+    if not os.path.exists('mydb.db'):
+        conn = sqlite3.connect('mydb.db')
+        create.main(conn)
+    else:
+        conn = sqlite3.connect('mydb.db')
+    userService = UsersService(conn)
+    # add friend service
+    # add post service
+
+    userInput = input("What would you like to do? ")
+    while userInput != "exit":
+        userInput = input("What would you like to do? ")
+        if userInput == "get all users":
+            users = userService.get_all_users()
+            for user in users:
+                print(user)
+
+
     return 0
 
 
