09f49428d52ce5f78237ecc070f1aea7a78bdb15
kamden-rasmussen <kamdenr@gmail.com>
2023-02-15 11:23:54 -0700

    done with automatic time stamp. did a little cleanup as well

--- create.py
+++ create.py
@@ -7,12 +7,30 @@ inject_users = injectUsers()
 def createTables():
     conn = sqlite3.connect('mydb.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS users
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)''')
-    c.execute('''CREATE TABLE IF NOT EXISTS posts
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, body TEXT, user_id INTEGER, created_at DATETIME, FOREIGN KEY(user_id) REFERENCES users(id))''')
-    c.execute('''CREATE TABLE IF NOT EXISTS friends
-                    (user_id INTEGER, friend_id INTEGER, following BOOL, FOREIGN KEY(user_id) REFERENCES users(id), FOREIGN KEY(friend_id) REFERENCES users(id))''')
+    c.execute('''CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name TEXT, 
+        email TEXT UNIQUE, 
+        password TEXT
+        )''')
+
+    c.execute('''CREATE TABLE IF NOT EXISTS posts(
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        body TEXT, user_id INTEGER, 
+        created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+        FOREIGN KEY(user_id) REFERENCES users(id)
+        )''')
+
+    c.execute('''CREATE TABLE IF NOT EXISTS friends(
+        user_id INTEGER, 
+        friend_id INTEGER, 
+        following BOOL, 
+        FOREIGN KEY(user_id) REFERENCES users(id), 
+        FOREIGN KEY(friend_id) REFERENCES users(id)
+        )''')
+
+    print("Tables created successfully")
+
     conn.commit()
     conn.close()
 
@@ -20,6 +38,9 @@ def insertUsers():
     conn = sqlite3.connect('mydb.db')
     c = conn.cursor()
     c.executemany('''INSERT INTO users (name, email, password) VALUES (?, ?, ?)''', inject_users)
+
+    print( "Users inserted successfully")
+
     conn.commit()
     conn.close()
 
@@ -31,9 +52,19 @@ def insertFriends():
         user2 = random.randint(1, 200)
         c.execute('''INSERT INTO friends (user_id, friend_id, following) VALUES (?, ?, 1)''', (user1, user2))
         c.execute('''INSERT INTO friends (user_id, friend_id, following) VALUES (?, ?, 1)''', (user2, user1))
+
+    print("Friends inserted successfully")
+
     conn.commit()
     conn.close()
 
+def insertPosts():
+    conn = sqlite3.connect('mydb.db')
+    c = conn.cursor()
+
+    c.execute('''INSERT INTO posts (body, user_id) VALUES (?, ?)''', (post, user))
+    conn.commit()
+    conn.close()
 
 def main():
     createTables()
